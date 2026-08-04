b2b6f4d8e101149b91f2ae493f0070859ec22a56
Eric Batista <eric.batista@indicium.ai>
2026-08-04 19:10:36 +0000

    removing fk columns that are no necessary

--- models/marts/geography/dim_geography.sql
+++ models/marts/geography/dim_geography.sql
@@ -3,10 +3,7 @@ with geography as (
     select
         geography_pk
         , city
-        , state_province_fk
-        , state_province_code
         , state_province_name
-        , country_region_code
         , country_region_name
 
     from {{ ref('int_geography') }}
